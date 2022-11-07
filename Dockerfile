de59e482c09ad96831f401273e03d9726a394e83
elghibaomar <elghibaomar@gmail.com>
2022-11-07 16:58:49 +0100

    l1

--- Dockerfile
+++ Dockerfile
@@ -15,8 +15,12 @@ RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 # 
 COPY ./app /code/app
 
+EXPOSE 8080
 # 
 # RUN python app/bin/download_model
 
+# CMD uvicorn app.sentiment_analyzer.api:app --host 0.0.0.0 --port 8000
 
-CMD ["uvicorn", "app.sentiment_analyzer.api:app", "--host", "0.0.0.0", "--port", $PORT]
+# CMD ["uvicorn", "app.sentiment_analyzer.api:app", "--host", "0.0.0.0", "--port", $PORT]
+
+CMD ["uvicorn", "app.sentiment_analyzer.api:app"]
--- app/sentiment_analyzer/api.py
+++ app/sentiment_analyzer/api.py
@@ -16,21 +16,21 @@ import requests
 
 app = FastAPI()
 
-origins = ['*']
+# origins = ['*']
 
 
-app.add_middleware(
-CORSMiddleware,
-allow_origins=["*"], # Allows all origins
-allow_credentials=True,
-allow_methods=["*"], # Allows all methods
-allow_headers=["*"], # Allows all headers
-)
+# app.add_middleware(
+# CORSMiddleware,
+# allow_origins=["*"], # Allows all origins
+# allow_credentials=True,
+# allow_methods=["*"], # Allows all methods
+# allow_headers=["*"], # Allows all headers
+# )
 
 
-@app.get('/')
-def hello():
-    return('hello')
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
 # class SentimentRequest(BaseModel):
